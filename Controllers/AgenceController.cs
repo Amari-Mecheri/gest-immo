8692f8f22fb728126aa2aaf29779f762b467868b
Amari MECHERI <amari.mecheri@gmail.com>
2023-09-20 01:35:21 +0100

    ajout de guard pour la navigation, persistance de l'utilisateur, passage de jwt token

---
+++ Controllers/AgenceController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gest_Immo_API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AgenceController : ControllerBase
+    {
+        [HttpGet("get-collaborators")]
+        public IActionResult Collaborators()
+        {
+            return Ok(new JsonResult(new { message = "Seuls les administrateurs d'agences peuvent voir la liste des collaborateurs" }));
+        }
+    }
+}
